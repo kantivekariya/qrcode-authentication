dc53d2368e30ccfa9e09c440b728e33eec36a52a
kantivekariya <kantivekariya242@gmail.com>
2022-09-27 16:02:37 +0530

    configure server

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import morgan from "morgan";
 import cors from "cors";
 import mainRouter from "./routes";
@@ -8,11 +10,15 @@ import config from "./config";
 import logger from "./services/logger";
 import { connectSocketIo } from "./config/socketconnect";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 app.use(cors());
 // Production environment
 const isProduction = config.server.environment === "production";
+
 app.use(bodyParser.json());
 
 //https debug
@@ -20,6 +26,10 @@ app.use(morgan("dev"));
 
 //Connect Mongo
 connectMongo();
+
+const publicDirectoryPath = path.join(__dirname, "../client/build");
+
+app.use(express.static(publicDirectoryPath));
 
 app.use("/", mainRouter);
 
