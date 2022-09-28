33e7ba7cceed85e33e3ebcf08d8e5f464ac6277a
kantivekariya <kantivekariya242@gmail.com>
2022-09-28 18:20:26 +0530

    update app routes

--- packages/server/app.js
+++ packages/server/app.js
@@ -32,11 +32,12 @@ const publicDirectoryPath = path.join(__dirname, "../client/build");
 app.use(express.static(publicDirectoryPath));
 
 
+app.use("/", mainRouter);
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
 })
 
-app.use("/", mainRouter);
 
 const server = app.listen(config.server.port, () => {
   logger.info(`Server is running on isProduction => ${isProduction}`);
